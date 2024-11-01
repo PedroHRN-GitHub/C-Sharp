5cfc7a8c27e91ed5b050fbe8344f335f6f56d48b
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-01 17:14:58 -0400

    Operadores Aritimeticos

---
+++ Operadores/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Operadores Aritimeticos
+
+double result = 12 + 5 +12/3;
+Console.WriteLine(result);
+
+
+double result2 = (12 + 5 +12)/3;
+Console.WriteLine(result2);
+
+
+double result3 = (2+8)*2/4;
+Console.WriteLine(result3);
+
