345e46d961f000688934e3bc00a3ce99068aef73
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-01 16:05:09 -0400

    Tipos Primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//tipos primitivos
+//Num inteiros
+
+short ano = 2024;
+
+int contador = 10;
+
+long PopuMundi = 80000000000000;
+
+//Num quebrados
+
+float temp = 43.6f;
+
+double PrecoProduto = 199.9
+
+//Caracteres
+
+char inicial = "P";
+
+//tipo logico
+
+bool Logado = true;
+
+bool ativo = false;
