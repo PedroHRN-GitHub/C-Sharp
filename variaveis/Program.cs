4bfd7b85edde181ec34b941685281b8277d87f70
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-01 16:29:19 -0400

    Tipos não Primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -22,3 +22,45 @@ char inicial = "P";
 bool Logado = true;
 
 bool ativo = false;
+
+//Tipos não Primitivos
+
+string saudacao = "Olá Mundo";
+
+//arrys
+
+int[] numeros = {1,2,3,4,5};
+
+string[] nomes = {"Pedro","Adriana","Carlos", "ana","ana c"}
+
+//classes 
+public class pessoas{
+    public string Nome;
+    public int idade;
+}
+
+// structs
+public class ponto{
+    public int y;
+    public int x;
+}
+
+//Enums
+
+public enum diadasemana{
+    Seg,Terc,Quart,Quint,Sexta,Sab,Domingo
+}
+
+//interface
+
+public interface IVeiculo{
+    void Mover();
+}
+
+//delegates
+
+public void delegates Processar(int numero);
+
+//NUll Types
+
+int? idade = null;
