87a42e77d7c795a95246798514906012580e5f8f
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-01 15:50:34 -0400

    0.0.0.0

---
+++ sla/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int i=0; i<=10;i++){
+    double x = 148.68;
+    double y = (x*i)/100;
+    double xy= x + y;
+    Console.WriteLine(xy);
+}
