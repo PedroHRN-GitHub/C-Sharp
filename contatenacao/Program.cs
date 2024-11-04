4736ce82a7c683ce8fcf7a79ac050e198e17a575
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-04 17:22:27 -0400

    Parse

--- contatenacao/Program.cs
+++ contatenacao/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 //COntatenacao
 string nome = "pedro";
 string saudacao = "olá, "+ nome +"! Bem Vindo.";
@@ -10,7 +10,17 @@ string nome1 = "pedro";
 string saudacao1 = $"Olá {nome1}! Bem-Vindo";
 
 Console.WriteLine(saudacao1);
-
+//GetType e Typeof
 int numero = 5;
 
 Console.WriteLine(numero.GetType());
+Console.WriteLine(typeof(int));
+
+
+//parse
+string valorTexto = "100";
+Console.WriteLine("A variável valorTexto é do tipo:"+ valorTexto.GetType());
+
+int numero1 = int.Parse(valorTexto);
+Console.WriteLine(numero1);
+Console.WriteLine("A variável valorTexto é do tipo:"+ valorTexto.GetType());
