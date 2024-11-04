16aa9cd92173d5e461c336e9f4fa49bc66938d34
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-04 15:26:52 -0400

    Operadores Lógicos
    Como alvo principal operadores lógicos, mas tbm foi add operadores de comparação e corrigido problemas em variáveis

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -12,3 +12,57 @@ Console.WriteLine(result2);
 double result3 = (2+8)*2/4;
 Console.WriteLine(result3);
 
+//[Operadores de comparação]
+
+//== para igual
+//!= para diferente
+//< menor que
+//> maior que
+//>= maior igual que
+//<= Menor igual que
+
+//Operadores Logicos
+
+//AND
+
+bool A = true;
+bool B = false;
+
+Console.WriteLine(A&&B);
+
+//OUR
+
+bool C = true;
+bool D = false;
+
+Console.WriteLine(C||D);
+
+//Not
+
+bool E = true;
+
+Console.WriteLine(!E);
+
+//XOR
+
+bool F = true;
+bool G = false;
+
+Console.WriteLine(F^G);
+
+//XNOR
+
+bool H = true;
+bool I = false;
+
+Console.WriteLine(!(H^I));
+
+//Teste de operadores
+
+bool L = true;
+bool P = false;
+bool K = false;
+
+bool resultado = !L ||(P&&K);
+Console.WriteLine(resultado);
+
--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,55 +1,73 @@
 ﻿//tipos primitivos
 //Num inteiros
 
+using System.Security.Cryptography;
+
 short ano = 2024;
+Console.WriteLine(ano);
 
 int contador = 10;
+Console.WriteLine(contador);
 
 long PopuMundi = 80000000000000;
+Console.WriteLine(PopuMundi);
 
 //Num quebrados
 
 float temp = 43.6f;
+Console.WriteLine(temp);
 
-double PrecoProduto = 199.9
+double PrecoProduto = 199.9;
+Console.WriteLine(PrecoProduto);
 
 //Caracteres
 
-char inicial = "P";
+char inicial = 'P';
+Console.WriteLine(inicial);
 
 //tipo logico
 
 bool Logado = true;
+Console.WriteLine(Logado);
 
 bool ativo = false;
+Console.WriteLine(ativo);
 
 //Tipos não Primitivos
+//NUll Types
+
+int? idade = null;
+Console.WriteLine(idade);
 
 string saudacao = "Olá Mundo";
+Console.WriteLine(saudacao);
 
 //arrys
 
 int[] numeros = {1,2,3,4,5};
+Console.WriteLine(numeros);
 
-string[] nomes = {"Pedro","Adriana","Carlos", "ana","ana c"}
+string[] nomes = {"Pedro","Adriana","Carlos", "ana","ana c"};
+Console.WriteLine(nomes);
 
 //classes 
-public class pessoas{
-    public string Nome;
+public class Pessoas{
+    public required string Nome;
     public int idade;
 }
 
 // structs
-public class ponto{
+public class Ponto{
     public int y;
     public int x;
 }
 
 //Enums
 
-public enum diadasemana{
+public enum Diadasemana{
     Seg,Terc,Quart,Quint,Sexta,Sab,Domingo
 }
+
 
 //interface
 
@@ -57,10 +75,7 @@ public interface IVeiculo{
     void Mover();
 }
 
+
 //delegates
 
-public void delegates Processar(int numero);
-
-//NUll Types
-
-int? idade = null;
+public delegate int Processar(int numero);
