0fad92e4f61caa35553aae2cd5b559c76f28a1b7
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-06 15:59:50 -0400

    add exemplo de casting

--- contatenacao/Program.cs
+++ contatenacao/Program.cs
@@ -24,3 +24,40 @@ Console.WriteLine("A variável valorTexto é do tipo:"+ valorTexto.GetType());
 int numero1 = int.Parse(valorTexto);
 Console.WriteLine(numero1);
 Console.WriteLine("A variável valorTexto é do tipo:"+ valorTexto.GetType());
+
+//convert\
+
+string valoremTexto = "100";
+Console.WriteLine("A variável valorTexto é do tipo:"+ valoremTexto);
+Console.WriteLine("A variável valorTexto é do tipo:"+ valoremTexto.GetType());
+
+int numeroCOnvert = Convert.ToInt32(valoremTexto);
+Console.WriteLine(numeroCOnvert);
+Console.WriteLine("A variável valorTexto é do tipo:"+ numeroCOnvert.GetType());
+
+//para string
+
+int numero2 = 234;
+Console.WriteLine(numero.GetType());
+
+string numbcomostring = numero2.ToString();
+Console.WriteLine(numbcomostring.GetType());
+
+//DAta e hora (personalizada)
+DateTime dataatual = DateTime.Now;
+Console.WriteLine(dataatual);
+
+string dataFormatada = dataatual.ToString("dd/MM/yyyy");
+Console.WriteLine(dataFormatada);
+
+string horaformatada = dataatual.ToString("HH:mm:ss");
+Console.WriteLine(horaformatada);
+
+//loc
+
+decimal valor = 1234.56m;
+string valorUS = valor.ToString("C", new System.Globalization.CultureInfo("en-us"));
+Console.WriteLine(valorUS);
+
+string valorBR = valor.ToString("C", new System.Globalization.CultureInfo("PT-BR"));
+Console.WriteLine(valorBR);
