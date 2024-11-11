499ad7cbe7d0429bdf04c0aef6eeeb5a911da951
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-11 14:31:49 -0400

    repetições

---
+++ Repetição/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random numbrand = new Random();
+int numb = numbrand.Next(1,20);
+int cont = 0;
+
+Console.WriteLine($"Numero Alvo: {numb}");
+
+while(cont != numb){
+
+    Console.WriteLine($"Contador: {cont}");
+    cont++;
+}
+
+Console.WriteLine($"Contador atingiu o número: {cont}");
