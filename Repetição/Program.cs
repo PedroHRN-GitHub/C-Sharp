d7b0081a065c41ee8ec7070b7efe1788a73b639a
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-11 15:56:03 -0400

    0.0.0.0.0

--- Repetição/Program.cs
+++ Repetição/Program.cs
@@ -1,14 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Random numbrand = new Random();
-int numb = numbrand.Next(1,20);
-int cont = 0;
 
-Console.WriteLine($"Numero Alvo: {numb}");
+//While
 
-while(cont != numb){
+// Random numbrand = new Random();
+// int numb = numbrand.Next(1,20);
+// int cont = 0;
 
-    Console.WriteLine($"Contador: {cont}");
-    cont++;
+// Console.WriteLine($"Numero Alvo: {numb}");
+
+// while(cont != numb){
+
+//     Console.WriteLine($"Contador: {cont}");
+//     cont++;
+// }
+
+// Console.WriteLine($"Contador atingiu o número: {cont}");
+
+//DO WHILE
+
+// int numero = 0;
+
+// do{
+//     Console.WriteLine($"Numero = {numero}");
+//     numero++;
+// }while(numero > 10);
+
+
+//For
+
+// for (int numero = 0; numero <=10; numero++){
+
+//     Console.WriteLine($"Numero = {numero}");
+// }
+
+//ForEach usado em array e listas
+
+string[] nomes = {"Pedro", "Pedra", "Pepe"};
+foreach (string nome in nomes){
+    Console.WriteLine(nome);
 }
-
-Console.WriteLine($"Contador atingiu o número: {cont}");
