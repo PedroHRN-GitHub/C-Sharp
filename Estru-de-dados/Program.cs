b5d6d11afc0efbd1ab512263896ea34aa0030502
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-18 16:48:17 -0400

    0.0.0.0.0

--- Estru-de-dados/Program.cs
+++ Estru-de-dados/Program.cs
@@ -46,7 +46,7 @@
 // }
 
 // HashSet<string> frutas = new HashSet<string> {"Maca","Banana","uva"};
-// frutas.Add("Pokan");
+// frutas.Add("Laranja");
 // frutas.Add("Maca");
 
 // Console.WriteLine("Elementos dos conjuntos");
@@ -57,3 +57,66 @@
 
 // Console.WriteLine("\n Verificacao de existencia:");
 // Console.WriteLine(frutas.Contains("Banana")?"Banana esta no conjunto" :"Banana nao esta no consjunto");
+
+// frutas.Remove("Laranja");
+// Console.WriteLine("\nApos remover laranja");
+// Console.WriteLine("Elementos dos conjuntos");
+// foreach (string i in frutas)
+// {
+//     Console.WriteLine(i);
+// }
+
+//atv 1
+
+// int[] Numb = {1,2,3,4,5};
+// int soma = 0;
+
+// for (int i = 0; i < Numb.Length; i++) 
+// { 
+//     soma += Numb[i];
+    
+// } 
+// Console.WriteLine(soma);
+
+//Atv 2
+
+// List<string> listanome = new List<string> {"wanderson", "jeane", "Ryan", "jeane", "Paula", "jeane"};
+// int Contnome = 0;
+
+// for (int i = 0; i <listanome.Count; i++){
+//     if(listanome[i] == "jeane"){
+//         Contnome++;
+//     }
+// }
+// Console.WriteLine(Contnome);
+
+//Atv 3
+
+// List<int> numeros = new List<int>{1,2,2,3,4,4,5};
+// HashSet<int> numerossemrep = new HashSet<int>(numeros);
+
+// foreach(int i in numerossemrep)
+// {
+//     Console.WriteLine(i);
+// }
+
+//Atv 4
+
+// HashSet<string> frutas = new HashSet<string> {"Maca","Banana","uva"};
+
+// Console.WriteLine(frutas.Contains("Banana") ? "Tem Banana" : "Nao tem banana");
+
+//Atv 5
+
+// List<int> numeros = new List<int>();
+
+// numeros.Add(1);
+// numeros.Add(2);
+// numeros.Add(3);
+// numeros.Add(4);
+// numeros.Add(5);
+
+// foreach(int i in numeros)
+// {
+//     Console.WriteLine(i);
+// }
