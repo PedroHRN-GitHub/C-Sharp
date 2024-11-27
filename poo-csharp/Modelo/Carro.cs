6b1c19f7792e5e79c780cb9f350a5fe0e1d30213
PedroHRN-GitHub <pedrohrngithub@gmail.com>
2024-11-27 15:53:42 -0400

    0.0.0.0.0

---
+++ poo-csharp/Modelo/Carro.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poo_csharp.Modelo
+{
+    public class Carro : Veiculo
+    {
+        // Atributos
+        private bool automatico { get; set; } 
+        private int numeroDePortas { get; set; }     
+
+        // Construtor
+        public Carro(string marca, string modelo, int ano, double velocidadeAtual, bool automatico, int numeroDePortas) : base (marca, modelo, ano, velocidadeAtual)
+        { 
+            this.automatico = automatico; 
+            this.numeroDePortas = numeroDePortas; 
+        }
+
+        // AbrirPortas é um método especifico da classe Carro.
+        public void AbrirPortas ()
+        {
+            Console.WriteLine("As portas do carro estão abertas.");
+        }
+
+        internal void Frear(int v1, bool v2, bool v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
